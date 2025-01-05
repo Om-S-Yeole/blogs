ef1b3c3f260170d49699d9d3fbeec0b214c6ff18
Om Yeole <omyeole01@gmail.com>
2025-01-05 20:25:36 +0530

    Adds feature for user to reset his password by email link sent if he forgot his password.

--- blog_website/blog_website/urls.py
+++ blog_website/blog_website/urls.py
@@ -33,6 +33,10 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('profile_update/', user_views.profile_update, name='profile_update'),
     path('profile/delete_account/', Delete_account.as_view(), name='delete_account'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
